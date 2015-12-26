5eae2923e640ee6004ef3eb8673c082f0b66dbda
RubinYeom <ytk.david@gmail.com>
2015-12-26 20:30:08 +0900

    array modi2

--- blog.js
+++ blog.js
@@ -8,7 +8,7 @@
         {
             title : "This is First Blog!",
             description : "Nothing to write"
-        }
+        },
         {
             title : "This is Second Blog!",
             description : "Oh Second~"
