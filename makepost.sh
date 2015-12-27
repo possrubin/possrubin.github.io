4c00afdadf94cd77578db869aba441a0cb61e582
RubinYeom <ytk.david@gmail.com>
2015-12-27 22:28:10 +0900

    hehhfeawh

--- makepost.sh
+++ makepost.sh
@@ -3,5 +3,8 @@
 filename=$1
 echo "<div id=\"title\">" >> $filename
 echo "</div>" >> $filename
+echo "<div id=\"writedate\">" >> $filename
+echo "$(date +'%Y-%m-%d %H:%M')" >> $filename
+echo "</div>" >> $filename
 echo "<div id=\"description\">" >> $filename
 echo "</div>" >> $filename
