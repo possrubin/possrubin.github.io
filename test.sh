b3287290905704d9798c786520634a87fa477012
RubinYeom <ytk.david@gmail.com>
2015-12-27 21:49:28 +0900

    dfafsfdssdf

--- test.sh
+++ test.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-echo "]"
+rm blogdata.json
+echo "" > blogdata.json
+echo "]" >> blogdata.json
 for f in *; do
     if [[ -d $f ]]; then
         for entry in $f/*; do
-            echo "{"
+            echo "{" >> blogdata.json
             arr=$(echo $entry | tr "/" ".")
             title=$(awk 'NR==2' $entry)
             convertedPath=${arr[0]}
             convertedPath=${convertedPath%?????}
-            echo "\"file\":\"$convertedPath\","
-            echo "\"title\":\"$title\""
-            echo "},"
+            echo "\"file\":\"$convertedPath\"," >> blogdata.json
+            echo "\"title\":\"$title\"" >> blogdata.json
+            echo "}," >> blogdata.json
         done
     fi
 done
-echo "]"
+sed -i '' -e '$ d' blogdata.json
+echo "}" >> blogdata.json
+echo "]" >> blogdata.json
