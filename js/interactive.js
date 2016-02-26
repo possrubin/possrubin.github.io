ad3f6270a8b3e6a26345593a5811d3bfda167de4
RubinYeom <ytk.david@gmail.com>
2016-02-26 16:42:01 +0900

    많은 변화

--- js/interactive.js
+++ js/interactive.js
@@ -1,4 +1,32 @@
 (function() {
     var app = angular.module('app', []);
+    app.controller('MenuController', function() {
+        this.menu = 1;
+        this.contents = pages;
+
+        this.setMenu = function(value) {
+            this.menu = value;
+        }
+
+        this.isMenu = function(value) {
+            return this.menu === value;
+        } 
+    )};
+
+
+    var pages = [
+        {
+            title: '헬로',
+            description: ''
+        },
+        {
+            title: '테스트',
+            description: ''
+        },
+        {
+            title: '하이',
+            description: ''
+        }
+    ];
 })();
 
